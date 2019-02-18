839064cc6d7f4f7cc24d579a22e70f437dfd1bb3
Taras Dashkevych <trdashix@gmail.com>
2019-02-18 15:47:25 -0800

    Add: translation functionality to the enqueue class;

--- inc/Base/Enqueue.php
+++ inc/Base/Enqueue.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Load needed styles and scripts.
+ * This class also loads translation files (might be changed later).
  *
  * @since 1.0.0
  * @package AssetsIntegration
@@ -19,7 +20,10 @@ class Enqueue {
      * @return void
      */
 	public function register() {
-		add_action( 'admin_enqueue_scripts', [ $this, 'adminEnqueue' ] );
+          // Load admin assets.
+          add_action( 'admin_enqueue_scripts', [ $this, 'adminEnqueue' ] );
+          // Load translation files.
+          add_action( 'plugins_loaded', [ $this, 'load_textdomain' ] );
 	}
 
 	/**
@@ -31,5 +35,16 @@ class Enqueue {
 	public function adminEnqueue() {
 		wp_enqueue_style( 'assets-integration-admin-style', ASSETS_INTEGRATION_PLUGIN_URL . 'assets/admin/css/style.css', array(), '1.0.0' );
 		wp_enqueue_script( 'assets-integration-admin-script', ASSETS_INTEGRATION_PLUGIN_URL . 'assets/admin/js/app.min.js', '1.0.0', true );
+     }
+     
+     /**
+	 * Load the plugin text domain.
+	 *
+	 * @since 1.0.0
+      * @return void
+	 */
+	public function load_textdomain() {
+          $lang_dir = dirname( plugin_basename( ASSETS_INTEGRATION_PLUGIN_FILE ) ) . '/languages/';
+          load_plugin_textdomain( 'my-plugin', false, $lang_dir );
 	}
 }
